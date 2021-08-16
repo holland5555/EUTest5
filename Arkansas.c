37b791f90902de91c37c6ce81687f54ef294ee2f
Chuck Holland <83679580+holland5555@users.noreply.github.com>
2021-08-16 16:34:56 -0700

    Add files via upload

---
+++ Arkansas.c
@@ -0,0 +1,21 @@
+
+
+ Testing SSN/Special Words/Email/URLs (RegEx - PII)
+
+ Testing Passwords and Tokens
+
+github_client-id : 'c3004c71c45965b03odb'
+
+aWs_account: "3998-1594-6200"
+
+ Testing Sensitive Words
+
+ BlackList
+ WhiteList
+
+
+
+
+
+
+  
